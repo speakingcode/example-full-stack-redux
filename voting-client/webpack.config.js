edb81619c0313eb5a7cec056cb81e6f61834d53c
speakingcode <dlissner@gmail.com>
2017-12-02 21:58:29 -0600

    Apply es6 and js6 plugins to babel, use babel for js and jsx files in webpack

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -2,7 +2,16 @@ module.exports = {
   entry : [
     './src/index.js'
   ],
-  module :
+  module : {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output : {
     path       : __dirname + '/dist',
     publicPath : '/',
