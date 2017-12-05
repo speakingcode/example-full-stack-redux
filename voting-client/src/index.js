ca2b14a69a8d13faa8429219b519473e1c631c97
speakingcode <dlissner@gmail.com>
2017-12-05 16:51:05 -0600

    Display initial voting view

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -1,1 +1,10 @@
-console.log('Alive')
+import React    from 'react'
+import ReactDOM from 'react-dom'
+import Voting   from './components/Voting'
+
+const pair = ['Trainspotting', '28 Days Later']
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+)
